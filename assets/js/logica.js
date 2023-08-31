ea7260a2b970dd58791fae041bf79c59d386350f
Gabigol dos croche <Ohlekaa@gmail.com>
2023-08-31 19:35:01 -0300

    initial commit

---
+++ assets/js/logica.js
@@ -0,0 +1,38 @@
+function generatePDF(){
+    var pdfObject = jsPDFInvoiceTemplate.default(props);
+    console.log("Object created: ", pdfObject);
+
+}
+    var props = {
+        outputType: jsPDFInvoiceTemplate.OutputType.Save,
+        returnJsPDFDocObject: true,
+        fileName: "Fotos da Impressora",
+        orientationLandscape: false,
+        compress: true,
+        logo: {
+            src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/logo.png",
+            type: 'PNG', //optional, when src= data:uri (nodejs case)
+            width: 53.33, //aspect ratio = width/height
+            height: 26.66,
+            margin: {
+                top: 0, //negative or positive num, from the current position
+                left: 0 //negative or positive num, from the current position
+            }
+        },
+        stamp: {
+            inAllPages: true, //by default = false, just in the last page
+            src: "https://raw.githubusercontent.com/edisonneza/jspdf-invoice-template/demo/images/qr_code.jpg",
+            type: 'JPG', //optional, when src= data:uri (nodejs case)
+            width: 20, //aspect ratio = width/height
+            height: 20,
+            margin: {
+                top: 0, //negative or positive num, from the current position
+                left: 0 //negative or positive num, from the current position
+            }
+        },
+        footer: {
+            text: "The invoice is created on a computer and is valid without the signature and stamp.",
+        },
+        pageEnable: true,
+        pageLabel: "Page ",
+    };
